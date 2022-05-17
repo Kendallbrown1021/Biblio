56f0722621867645058a93b58de24f3c993d3cfb
Kendallbrown1021 <kendall.darnel.brown@gmail.com>
2022-05-17 06:10:34 -0700

    adding new changes

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -20,31 +20,51 @@ const Login = () => {
       <div className="login_container">
         <h1>Bibliolater</h1>
         <div className="login_form-contianer">
-          <form className>
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              onChange={(event) => handle(event)}
-              id="name"
-              value={data.name}
-            />
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              onChange={(event) => handle(event)}
-              id="username"
-              value={data.username}
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              onChange={(event) => handle(event)}
-              id="password"
-              value={data.password}
-            />
+          <form className="login_form">
+            <div className="login_input-container">
+              {" "}
+              <label for="name" className="form_label">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                onChange={(event) => handle(event)}
+                id="name"
+                value={data.name}
+                className="form_input"
+              />
+            </div>
+            <div className="login_input-container">
+              <label for="username" className="form_label">
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={(event) => handle(event)}
+                id="username"
+                value={data.username}
+                className="form_input"
+              />
+            </div>
+            <div className="login_input-container">
+              <label for="password" className="form_label">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={(event) => handle(event)}
+                id="password"
+                value={data.password}
+                className="form_input"
+              />
+            </div>
+
             <button className="bg-gray-500">Submit Form</button>
           </form>
           ;
